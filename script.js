ebcfd47cd34cb64aa3eb661dfb48d58e7a9c534c
DZMITRY YATSKEVICH <53887281+GoodFellow23@users.noreply.github.com>
2020-03-17 16:02:19 +0300

    init: add  screen switch

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ const sticky = header.offsetTop;
 const headerCont = document.getElementById('header__content');
 const headerBottom = document.getElementById('header-bottom');
 const slider = document.getElementById('slider');
+let numSlider = 0;
+const scrolls = document.getElementsByClassName('slider__scroll');
 
+
+//Navigation 
 nav.addEventListener('click', (event) => {
     nav.querySelectorAll('.header__navigation__link').forEach(el => {
         el.classList.remove('header__navigation--selected');
@@ -12,6 +16,7 @@ nav.addEventListener('click', (event) => {
     event.target.classList.add('header__navigation--selected');
 });
 
+//Sticky header
 window.onscroll = () => {
     if (window.pageYOffset > sticky + 40) {
         header.classList.add('sticky-header');  
@@ -24,6 +29,7 @@ window.onscroll = () => {
     }
 };
 
+//Переключение экранов
 slider.addEventListener('click',(event) => {
     if(event.target.classList.contains('iphone__vertical__screen')) {
        if(event.target.style.backgroundImage == 'none') {
@@ -41,3 +47,45 @@ slider.addEventListener('click',(event) => {
      }
 });
 
+//Карусель
+
+for ( el  of scrolls)  {
+    el.addEventListener('click',(event) => {
+        if(numSlider == 0) {
+            numSlider++;
+            slider.style.backgroundImage = "url('./assets/img/Slide-2.png')";
+            slider.style.justifyContent = "space-between";
+            document.getElementById('iphone-vertical').style.display = "none";
+            document.getElementById('iphone-horizontal').style.display = "none";
+            document.querySelector('.bg-bottom').style.background = '#648BF0';
+        } else if (numSlider == 1) {
+            numSlider--;
+            slider.style.backgroundImage = "initial";
+            document.getElementById('iphone-vertical').style.display = "initial";
+            document.getElementById('iphone-horizontal').style.display = "initial";
+            document.querySelector('.bg-bottom').style.background = 'initial';
+            slider.querySelector('.iphone__vertical__screen').style.backgroundImage = "url('./assets/img/bg-vertical.png')";    
+            slider.querySelector('.iphone__horizontal__screen').style.backgroundImage = "url('./assets/img/bg-horizontal.png')";    
+        }
+    });
+}
+
+// scrollLeft.addEventListener('click',(event) => {
+//     console.log('start');
+//     if(numSlider == 0) {
+//         console.log('in');
+//         numSlider++;
+//         slider.style.backgroundImage = "url('./assets/img/Slide-2.png')";
+//         slider.style.justifyContent = "space-between";
+//         document.getElementById('iphone-vertical').style.display = "none";
+//         document.getElementById('iphone-horizontal').style.display = "none";
+//         document.getElementById('');
+//     } else if (numSlider == 1) {
+//         numSlider--;
+//         slider.style.backgroundImage = "initial";
+//         document.getElementById('iphone-vertical').style.display = "initial";
+//         document.getElementById('iphone-horizontal').style.display = "initial";
+
+//     }
+//     console.log('end');
+// });
